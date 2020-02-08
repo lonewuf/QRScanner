d58c644be207a60929ecc74c58139d30f4f999d9
tawbuts <john.nico.austria@gmail.com>
2020-02-08 16:49:43 +0800

    edit other files

--- app/src/main/java/com/example/priyanka/qrbarcodescanner/AboutActivity.java
+++ app/src/main/java/com/example/priyanka/qrbarcodescanner/AboutActivity.java
@@ -1,7 +1,11 @@
 package com.example.priyanka.qrbarcodescanner;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class AboutActivity extends AppCompatActivity {
 
@@ -9,5 +13,34 @@ public class AboutActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar_about);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("SSS Village");
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_toolbar, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        Intent intent;
+
+        switch (item.getItemId()) {
+
+            case R.id.action_admin:
+                intent = new Intent(AboutActivity.this, SelectYearActivity.class);
+                startActivity(intent);
+                return true;
+            case R.id.action_about:
+                intent = new Intent(AboutActivity.this, AboutActivity.class);
+                startActivity(intent);
+                return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
